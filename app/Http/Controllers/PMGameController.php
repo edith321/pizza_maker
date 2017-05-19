1d64aed269c783d4fab0a3626f3add06a786ee46
Edita <edita.paulikaite@inbox.lt>
2017-05-19 15:41:33 +0300

    Migratio adds resources_id field

--- app/Http/Controllers/PMGameController.php
+++ app/Http/Controllers/PMGameController.php
@@ -18,6 +18,7 @@ class PMGameController extends Controller
     {
         return view('front-end.game');
     }
+
     /**
      * Store the uploaded image.
      * POST /pmcheese
---
+++ database/migrations/2017_05_19_121642_add_new_field_to_ingredients_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNewFieldToIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pm_ingredients', function (Blueprint $table) {
+            $table->string('resource_id', 36)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pm_ingredients', function (Blueprint $table) {
+            $table->dropColumn('resource_id');
+        });
+    }
+}
